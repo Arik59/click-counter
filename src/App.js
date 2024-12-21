506078f61c6038ee91e459cb4a94117bbd6837cd
Arik59 <kruftik203@gmail.com>
2024-12-21 16:24:00 +0200

    made counter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(){
-  return <div>Hi, SSSS</div>
+  const [count, setCount] = useState(0);
+
+  const increase = () => setCount(count + 1);
+  const decrease = () => setCount(count - 1);
+  const takeoff = () => setCount(0)
+
+  if (count < 0) {
+    alert("Nooooooooooooooooooooooooo!")
+    takeoff();
+  }
+
+  return (
+  <div style = {styles.container}>
+    <h1 style = {styles.heading}>clicker</h1>
+    <p style = {styles.counter}>Clicks: {count}</p>
+    <div>
+      <button style={styles.buttons} onClick={increase}>Increase</button>
+      <button style={styles.buttons} onClick={decrease}>Decrease</button>
+      <button style={styles.buttons} onClick={takeoff}>Take off</button>
+    </div>
+  </div>
+);
 }
+
+//Прості стилі
+const styles = {
+  container: {
+    textAlign: "center",
+    marginTop: "100px"
+  },
+  heading: {
+    color: "#ff0000"
+  },
+  counter: {
+    fontSize: "30px"
+  },
+  buttons: {
+    padding: "10px 20px",
+    margin: "5px"
+  }
+}
+
+
 export default App;
